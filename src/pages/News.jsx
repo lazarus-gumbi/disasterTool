781aaf3fc5ff4c9cf5f4e24b6d26f5694abb46f4
lazarus-gumbi <gumbimelusi2@gmail.com>
2023-04-10 17:30:06 +0200

    news are fetched from api

--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import NewsCard from '../components/NewsCard'
+import { url } from '../components/secrets'
 
 function News() {
+
+    const [news, setNews] = useState([])
+
+    useEffect(() => {
+      fetch(url)
+      .then(response=> response.json())
+      .then(data=>setNews(data.data))
+    
+    }, [])
+    
+
   return (
     <div className="h-screen bg-[url('./assets/imgs/disaster.jpg')] bg-cover">
         <div className='h-1/4 w-full flex justify-center items-center'>
@@ -11,9 +23,13 @@ function News() {
             </form>
         </div>
         <div className='h-3/4 flex items-center justify-evenly'>
-            <NewsCard/>
-            <NewsCard/>
-            <NewsCard/>
+            
+            {news.slice(0,3).map(i =>{
+                return <NewsCard img_url={i.image?i.image:"https://images.unsplash.com/photo-1586339949916-3e9457bef6d3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"} headline={i.title} description={i.description.substring(0,150)} url={i.url} category={i.category}/>
+            
+            })}
+
+            
         </div>
     </div>
   )
